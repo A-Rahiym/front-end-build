08e155d79fa1f1fced167958a431551c371928f8
A-Rahiym <141669620+Araheym@users.noreply.github.com>
2024-09-27 22:31:15 +0100

    navigation and hero section completed

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Hero.scss'
+const Hero = () => {
+  return (
+    <div className='Hero-section-container'>
+      <div className='Hero-section'>
+        <h3>Start up</h3>
+        <h1>Forget about Code</h1>
+        <p>
+        
+          Startup Framework gives you complete freedom over your creative <br />
+          process — you don’t have to think about any technical aspects. <br />
+          There are no limits and absolutely no coding. </p>
+        <div className='circle-container'>
+        <div className='circle-shape'/>
+        <div className='circle-shape hidden'/>
+        <div className='circle-shape hidden'/>        
+        <div className='circle-shape hidden'/>
+        <div className='circle-shape hidden'/>
+        <div className='circle-shape hidden'/>
+        </div>
+        <button>Create an Account</button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navigation.scss'
+const Navigation = () => {
+    return (
+        <div className='nav-link-container'>
+            <div className='nav-links'>
+                <a href="/">Home</a>
+                <a href="/">pricing</a>
+                <a href="/">Feature</a>
+                <a href="/">Blog</a>
+                <a href="/">contact us</a>
+            </div>
+        </div>
+    )
+}
+
+export default Navigation
